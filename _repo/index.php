49dab19ace724514f34f788fe814be5b603fe4bc
SFFScorpion <jaystu138@gmail.com>
2019-03-24 14:28:19 -0400

    im tryin

---
+++ _repo/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Scorpion Repository</title>
+  </head>
+    <h1>Scorpion Repository</h1>
+  <body>
+    <ul>
+		<li><a href="plugin.program.scorpionwizard">Scorpion Wizard</a></li>
+		<li><a href="repository.scorpion">Scorpion Repository</a></li>
+		<li><a href="plugin.video.scorpion">Scorpion Video Addon</a></li>
+		<li><a href="skin.Scorpion">Scorpion Skin</a></li>
+  	</ul>
+  </body>
+</html>
